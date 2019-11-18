c3e17e7bd70aec58df61bf1675b87042b5ac3904
EleazarTracana <eleazartracana@gmail.com>
2019-11-18 13:37:53 -0800

    se acomoda lo pushee mal

--- client/Servidor/Server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const cors    = require('cors')
-const morgan  = require('morgan')
-
-const app = express()
-app.use(morgan('combine'))
-app.use(cors());
-
-app.listen(process.env.PORT || 8081)
