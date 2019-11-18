7dd03ba191736b5666aaa9c79029375628970e8a
EleazarTracana <eleazartracana@gmail.com>
2019-11-18 13:34:10 -0800

    se anade el proyecto con el servidor

---
+++ client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+
+<body>
+  <div id="app">
+    <router-view/>
+  </div>
+</body>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body { 
+  background: url(https://cdn.24.co.za/files/Cms/General/d/8033/07653b2c234b4b538c23909d7fd70011.jpg) 
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
